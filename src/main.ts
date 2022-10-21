32d7033f18e1371b8cdaf2782b52c53898d888bd
Sejmou <samo.kolter@gmail.com>
2022-10-21 12:38:15 +0200

    making sure main.ts works

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
 import './style.css';
+
+const exampleHeading = document.createElement('h1');
+exampleHeading.textContent = 'Hi! This heading was created in main.ts';
+
+document.body.appendChild(exampleHeading);
